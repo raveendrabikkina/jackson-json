74d4560865a4df7cc78abdea1ad10b43957a479b
raveendrabikkina <braveendra@gmail.com>
2019-08-18 17:58:14 +1000

    New.

---
+++ src/main/java/com/ravi/jacksonjson/JacksonJsonApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.jacksonjson;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JacksonJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JacksonJsonApplication.class, args);
+    }
+
+}
